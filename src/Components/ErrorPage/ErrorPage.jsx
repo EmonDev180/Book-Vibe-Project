4caed73c5b314f5c7bcb2df3ee9bd895532f18da
Emon Dev180 <shuvroemon.cse@gmail.com>
2025-04-13 08:40:08 +0600

    Navber commit

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='h-screen flex justify-center items-center '>
+            <h1 className='text-3xl font-bold'>Page not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
